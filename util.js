9832aa4f71ab3cfeb6cd069b677e41339e68f528
Nicky Nym <54910081+nicky-nym@users.noreply.github.com>
2019-10-31 13:00:13 -0700

    Update util.js

--- util.js
+++ util.js
@@ -6,26 +6,32 @@
 // This is free and unencumbered software released into the public domain.
 // For more information, please refer to <http://unlicense.org>
 
-function * upto (n) {
-  for (let i = 0; i < n; i++) {
+function * count ({ to, from = 0 } = {}) {
+  for (let i = from; i < to; i++) {
     yield i
   }
+}
+
+function * countTo (to) {
+  count({ to: to })
 }
 
 function randomInt (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-function nudge (xyz, { dx = 0, dy = 0, dz = 0, dxyz = [0, 0, 0] }) {
-  let x = 0
-  let y = 0
-  let z = 0
-  let dX = 0
-  let dY = 0
-  let dZ = 0
-  ;[x, y, z] = xyz
-  ;[dX, dY, dZ] = dxyz
+function xyz (x, y, z = 0) {
+  return [x, y, z]
+}
+
+function rgba (r, g, b, a = 0) {
+  return [r, g, b, a]
+}
+
+function nudge (xyz, { dx = 0, dy = 0, dz = 0, dxyz = [0, 0, 0] } = {}) {
+  const [x, y, z] = xyz
+  const [dX, dY, dZ] = dxyz
   return [x + dx + dX, y + dy + dY, z + dz + dZ]
 }
 
-export { upto, randomInt, nudge }
+export { xyz, rgba, count, countTo, randomInt, nudge }
