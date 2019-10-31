1fd82a9ebb9f93bfe1391e1a4052314b0982b582
Nicky Nym <54910081+nicky-nym@users.noreply.github.com>
2019-10-31 15:46:29 -0700

    Update util.js

--- util.js
+++ util.js
@@ -34,4 +34,18 @@ function nudge (xyz, { dx = 0, dy = 0, dz = 0, dxyz = [0, 0, 0] } = {}) {
   return [x + dx + dX, y + dy + dY, z + dz + dZ]
 }
 
-export { xyz, rgba, count, countTo, randomInt, nudge }
+function xy2xyz (xyz, deltaZ = 0) {
+  const X = 0
+  const Y = 1
+  const Z = 2
+  // z = xyz[Z] + delta_z if len(xyz) > 2 else delta_z
+  const z = xyz.length > 2 ? xyz[Z] + deltaZ : deltaZ
+  return [xyz[X], xyz[Y], z]
+}
+
+function yzwh2rect (y, z, width, height) {
+  // [(3, 2), (8, 2), (8, 6), (3, 6)] == yzwh2rect(3, 2, 5, 4)
+  return [xyz(y, z), xyz(y + width, z), xyz(y + width, z + height), xyz(y, z + height)]
+}
+
+export { xyz, rgba, count, countTo, randomInt, nudge, xy2xyz, yzwh2rect }
