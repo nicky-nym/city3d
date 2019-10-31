4afbac6b2c9c918c0e3ae86de8532e0eebad326e
Nicky Nym <54910081+nicky-nym@users.noreply.github.com>
2019-10-31 13:05:41 -0700

    Add files via upload

---
+++ output.js
@@ -0,0 +1,13 @@
+// output.js
+//
+// Authored in 2019 at <https://github.com/nicky-nym/city3d>
+
+// UNLICENSE
+// This is free and unencumbered software released into the public domain.
+// For more information, please refer to <http://unlicense.org>
+
+function print (str) {
+  console.log(str)
+}
+
+export { print }
