c16ba69d90abd715e4d4f5cde07d47b4a41bbff4
nicky-nym <nym.nicky@gmail.com>
2019-12-09 10:50:37 -0800

    added windows to Highrise buildings in Manhattan

--- content/buildings/highrise.js
+++ content/buildings/highrise.js
@@ -6,7 +6,7 @@
   */
 
 import { UNIT } from '../../src/core/unit.js'
-import { xyz } from '../../src/core/util.js'
+import { xy, xyz } from '../../src/core/util.js'
 import { Building } from '../../src/architecture/building.js'
 
 const HIGHRISE_BUILDING_SPEC = {
@@ -15,6 +15,15 @@ const HIGHRISE_BUILDING_SPEC = {
   offset: xyz(0, 0, 0),
   numStoreys: { type: 'randomInt', min: 4, max: 60 },
   shape: { type: 'rectangle', data: null }, // rectangle size is set in constructor
+  walls: [{
+    windows: [{ type: 'rectangle', data: null, at: xy(1, 3) }]
+  }, {
+    windows: [{ type: 'rectangle', data: null, at: xy(1, 3) }]
+  }, {
+    windows: [{ type: 'rectangle', data: null, at: xy(1, 3) }]
+  }, {
+    windows: [{ type: 'rectangle', data: null, at: xy(1, 3) }]
+  }],
   roof: {
     parapetHeight: UNIT.feet(4)
   }
@@ -26,6 +35,11 @@ const HIGHRISE_BUILDING_SPEC = {
 class Highrise extends Building {
   makeBuilding (size, at = { x: 0, y: 0 }) {
     HIGHRISE_BUILDING_SPEC.shape.data = size
+    for (const wallSpec of HIGHRISE_BUILDING_SPEC.walls) {
+      for (const windowSpec of wallSpec.windows) {
+        windowSpec.data = xy(size.x - 2, 5.5)
+      }
+    }
     return super.makeBuildingFromSpec(HIGHRISE_BUILDING_SPEC, at)
   }
 }
