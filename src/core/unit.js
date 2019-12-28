effba285fb8f206583f9ba3de4e026cd04a8e73a
nicky-nym <nym.nicky@gmail.com>
2019-12-27 16:03:36 -0800

    in UNIT, refactored to make the code a bit more DRY

--- src/core/unit.js
+++ src/core/unit.js
@@ -5,14 +5,7 @@
  * For more information, please refer to <http://unlicense.org>
  */
 
-// distances
-const FEET_PER_METER = 3.28084
-const FEET_PER_KILOMETER = 3280.84
-const FEET_PER_FOOT = 1
-
-// angles
-const DEGREES_PER_RADIAN = 180 / Math.PI
-const DEGREES_PER_DEGREE = 1
+const UNIT = {}
 
 /**
  * Returns a value in a new unit of measure from a given value.
@@ -20,8 +13,30 @@ const DEGREES_PER_DEGREE = 1
  * @param {function} to - a unit of measure, such as UNIT.km
  * @returns {number} the measurement, after converting to the new units
  */
-function convert ({ from, to }) {
-  return 1 / to(1 / from)
+UNIT.convert = ({ from, to }) => 1 / to(1 / from)
+
+/**
+ * Defines a new unit of measure, such as "meters per second squared".
+ * @param {string} displayName - a descriptive name to show an end-user
+ * @param {function} func - a function that convert a value of this unit to the corresponding value in the canonical units for this physical property
+ * @returns {function} the same func that was given as an arg
+ */
+UNIT.define = function (displayName, func) {
+  func.displayName = displayName
+  return func
+}
+
+// For distances, we use FEET as the canonical unit
+const _FEET = {
+  PER_METER: 3.28084,
+  PER_KILOMETER: 3280.84,
+  PER_FOOT: 1
+}
+
+// For angles, we use DEGREES as the canonical unit
+const _DEGREES = {
+  PER_RADIAN: 180 / Math.PI,
+  PER_DEGREE: 1
 }
 
 /**
@@ -29,85 +44,62 @@ function convert ({ from, to }) {
  * @param {number} length - a measurement in meters
  * @returns {number} the measurement in feet
  */
-function meters (length) {
-  return length * FEET_PER_METER
-}
-meters.displayName = 'meters'
+UNIT.meters = UNIT.define('meters', length => length * _FEET.PER_METER)
 
 /**
  * Returns a value in feet when given a value in kilometers.
  * @param {number} length - a measurement in kilometers
  * @returns {number} the measurement in feet
  */
-function km (length) {
-  return length * FEET_PER_KILOMETER
-}
-km.displayName = 'km'
+UNIT.km = UNIT.define('km', length => length * _FEET.PER_KILOMETER)
 
 /**
  * Returns a value in feet when given a value in feet.
  * @param {number} length - a measurement in feet
  * @returns {number} the measurement in feet
  */
-function feet (length) {
-  return length * FEET_PER_FOOT
-}
-feet.displayName = 'feet'
+UNIT.feet = UNIT.define('feet', length => length * _FEET.PER_FOOT)
 
 /**
  * Returns a value in degrees when given a value in radians.
  * @param {number} angle - a measurement in radians
  * @returns {number} the measurement in degrees
  */
-function radians (angle) {
-  return angle * DEGREES_PER_RADIAN
-}
-radians.displayName = 'radians'
+UNIT.radians = UNIT.define('radians', angle => angle * _DEGREES.PER_RADIAN)
 
 /**
  * Returns a value in degrees when given a value in degrees.
  * @param {number} angle - a measurement in degrees
  * @returns {number} the measurement in degrees
  */
-function degrees (angle) {
-  return angle * DEGREES_PER_DEGREE
-}
-degrees.displayName = 'degrees'
+UNIT.degrees = UNIT.define('degrees', angle => angle * _DEGREES.PER_DEGREE)
 
-// TODO: code review: reconsider whether we really want this
-function count (count) {
-  return count
-}
-count.displayName = 'count'
+/**
+ * Returns an integer count.
+ * @param {number} count - an integer count
+ * @returns {number} an integer count
+ */
+UNIT.count = UNIT.define('count', count => count)
 
-// TODO: code review: reconsider whether we really want this
-function squareFeet (area) {
-  return area
-}
-squareFeet.displayName = 'square feet'
+/**
+ * Returns a value in square feet when given a value in square feet.
+ * @param {number} area - a measurement in square feet
+ * @returns {number} the measurement in square feet
+ */
+UNIT.squareFeet = UNIT.define('square feet', area => area)
 
-// TODO: code review: reconsider whether we really want this
-function numberPerSquareFoot (average) {
-  return average
-}
-numberPerSquareFoot.displayName = 'per square foot'
+/**
+ * Returns a value in quantity per square foot when given a value in quantity per square foot.
+ * @param {number} average - a measurement in quantity per square foot
+ * @returns {number} the measurement in quantity per square foot
+ */
+UNIT.numberPerSquareFoot = UNIT.define('per square foot', average => average)
 
-// TODO: code review: reconsider whether we really want this
-function ratio (float) {
-  return float
-}
-ratio.displayName = 'ratio'
+/**
+ * Returns a ratio value when given a ratio value.
+ * @param {float} float - a ratio value
+ * @returns {float} the ratio value
+ */
+UNIT.ratio = UNIT.define('ratio', float => float)
 
-const UNIT = {
-  convert,
-  meters,
-  km,
-  feet,
-  radians,
-  degrees,
-  count,
-  squareFeet,
-  numberPerSquareFoot,
-  ratio
-}
 export { UNIT }
