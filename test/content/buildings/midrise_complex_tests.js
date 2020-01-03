984e373d90f83cd4fa8986a63e166bf931561d8e
nicky-nym <nym.nicky@gmail.com>
2020-01-03 11:38:24 -0800

    fixed #13: MidriseComplex tests include basic sanity checks of Metrics

--- test/content/buildings/midrise_complex_tests.js
+++ test/content/buildings/midrise_complex_tests.js
@@ -7,6 +7,7 @@
 
 import { Byway } from '../../../src/architecture/byway.js'
 import { City } from '../../../src/architecture/city.js'
+import { METRIC } from '../../../src/architecture/metric.js'
 import { MidriseComplex } from '../../../content/buildings/midrise_complex.js'
 import { Storey } from '../../../src/architecture/storey.js'
 import { Roof } from '../../../src/architecture/roof.js'
@@ -62,6 +63,41 @@ describe('MidriseComplex', function () {
 
       complex.accept(node => { count += node instanceof Byway ? 1 : 0 })
       count.should.be.at.least(1)
+    })
+  })
+
+  describe('#getValueForMetric', function () {
+    const complex = new MidriseComplex({ numRowPairs: 1, numColPairs: 1 })
+
+    it('should have the correct GROSS_FLOOR_AREA', function () {
+      complex.getValueForMetric(METRIC.GROSS_FLOOR_AREA).should.equal(37134)
+    })
+
+    it('should have the correct WALL_AREA', function () {
+      complex.getValueForMetric(METRIC.WALL_AREA).should.be.closeTo(21551, 1)
+    })
+
+    it('should have the correct WINDOW_AREA', function () {
+      const windowsPerWall = 8
+      const wallsPerStorey = 4
+      const aveStoreysPerTower = 3.5
+      const numTowers = 4
+      const expectedWindowCount = windowsPerWall * wallsPerStorey * aveStoreysPerTower * numTowers
+      const areaPerWindow = 2.5 * 5
+      const expectedWindowArea = expectedWindowCount * areaPerWindow
+
+      const doorsPerStorey = 2
+      const expectedDoorCount = doorsPerStorey * aveStoreysPerTower * numTowers
+      const areaPerDoor = 3 * (6 + 8 / 12)
+      const expectedDoorArea = expectedDoorCount * areaPerDoor
+
+      const expectedArea = expectedWindowArea + expectedDoorArea
+      complex.getValueForMetric(METRIC.WINDOW_AREA).should.equal(expectedArea)
+    })
+
+    // Turn this on when we generate realistic numbers for DAYLIGHT_FACTOR_ESTIMATE
+    it.skip('should have the correct DAYLIGHT_FACTOR_ESTIMATE', function () {
+      complex.getValueForMetric(METRIC.DAYLIGHT_FACTOR_ESTIMATE).should.equal(-1)
     })
   })
 })
