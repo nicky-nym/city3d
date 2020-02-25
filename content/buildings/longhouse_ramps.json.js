cd9f5f35496be5923263b82e38332bd79ba0e871
nicky-nym <nym.nicky@gmail.com>
2020-02-25 13:44:34 -0800

    added missing walkways to longhouse_ramps.json.js

--- content/buildings/longhouse_ramps.json.js
+++ content/buildings/longhouse_ramps.json.js
@@ -11,32 +11,33 @@ export default /* eslint-disable */
   unit: 'feet',
   anchorPoint: { x: 0, y: 0, z: 0 },
   comments: [
-    '                                    ',
-    '                                    ',
-    '      ab                            ',
-    '      | \          r     l--m       ',
-    '      e  \               | /        ',
-    '       \  \   q       o  n/         ',
-    '        \  \      p                 ',
-    '         d--c--h                    ',
-    '         |  |  |                    ',
-    '         |  |  |                    ',
-    '         |  |  |                    ',
-    '         g--f--i-j-k                ',
-    '         |         |                ',
-    '         |         |                ',
-    '         G--F--I-J-K                ',
-    '         |  |  |                    ',
-    '         |  |  |                    ',
-    '         |  |  |                    ',
-    '         D--C--H                    ',
-    '        /  /       P                ',
-    '       /  /   Q       O  N\         ',
-    '      E  /               | \        ',
-    '      | /          R     L--M       ',
-    '      AB                            ',
-    '                                    ',
-    '                                    '
+    '                                                      ',
+    '                                                      ',
+    '      ab                                              ',
+    '      | \          r     l--m                         ',
+    '      e  \               | /                          ',
+    '       \  \   q       o  n/                           ',
+    '        \  \      p                                   ',
+    '         d--c--h                                      ',
+    '         |  |  |                                      ',
+    '         |  |  |                                      ',
+    '         |  |  |                                      ',
+    '         g--f--i-j-k--s                               ',
+    '         |         u--t                               ',
+    '         |         |                                  ',
+    '         |         U--T                               ',
+    '         G--F--I-J-K--S                               ',
+    '         |  |  |                                      ',
+    '         |  |  |                                      ',
+    '         |  |  |                                      ',
+    '         D--C--H                                      ',
+    '        /  /       P                                  ',
+    '       /  /   Q       O  N\    X--Z0--Z1--Z2--Z3--W   ',
+    '      E  /               | \   |                  |   ',
+    '      | /          R     L--M  Y------------------V   ',
+    '      AB                                              ',
+    '                                                      ',
+    '                                                      '
   ],
   def: {
     /* exit from highroad (toward landing) */
@@ -67,6 +68,38 @@ export default /* eslint-disable */
     Q: { x: 60, y: 55 },
     R: { x: 80, y: 40 },
 
+    /* landing, south access walkway */
+    S: { x: 129, y: 279 },
+    T: { x: 129, y: 285 },
+    U: { x: 85, y: 285 },
+
+    /* lower plaza */
+    V: { x: 490, y: 30 },
+    W: { x: 490, y: 45 },
+    X: { x: 170, y: 45 },
+    Y: { x: 170, y: 30 },
+
+    /* lower plaza access walkways */
+    Z0a: { x: (96 * 0) + 183 + 6, y: 45 },
+    Z0b: { x: (96 * 0) + 183 + 6, y: 129 },
+    Z0c: { x: (96 * 0) + 183, y: 129 },
+    Z0d: { x: (96 * 0) + 183, y: 45 },
+
+    Z1a: { x: (96 * 1) + 183 + 6, y: 45 },
+    Z1b: { x: (96 * 1) + 183 + 6, y: 129 },
+    Z1c: { x: (96 * 1) + 183, y: 129 },
+    Z1d: { x: (96 * 1) + 183, y: 45 },
+
+    Z2a: { x: (96 * 2) + 183 + 6, y: 45 },
+    Z2b: { x: (96 * 2) + 183 + 6, y: 129 },
+    Z2c: { x: (96 * 2) + 183, y: 129 },
+    Z2d: { x: (96 * 2) + 183, y: 45 },
+
+    Z3a: { x: (96 * 3) + 183 + 6, y: 45 },
+    Z3b: { x: (96 * 3) + 183 + 6, y: 129 },
+    Z3c: { x: (96 * 3) + 183, y: 129 },
+    Z3d: { x: (96 * 3) + 183, y: 45 },
+
     /* entrance to highroad (from landing) */
     a: { x: 30, y: 670 - 0 },
     b: { x: 35, y: 670 - 0 },
@@ -94,7 +127,12 @@ export default /* eslint-disable */
     p: { x: 73, y: 670 - 69 },
     q: { x: 60, y: 670 - 55 },
     r: { x: 80, y: 670 - 40 },
-    
+
+    /* landing, north access walkway */
+    s: { x: 129, y: 670 - 279 },
+    t: { x: 129, y: 670 - 285 },
+    u: { x: 85, y: 670 - 285 },
+
   },
   storeys: [{
     name: 'exit from highroad (toward landing)',
@@ -175,6 +213,21 @@ export default /* eslint-disable */
       }
     }]
   }, {
+    name: 'landing, south access walkway',
+    altitude: 15,
+    incline: 2.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/K' },
+          { $ref: '#/def/S' },
+          { $ref: '#/def/T' },
+          { $ref: '#/def/U' }
+        ]
+      }
+    }],
+  }, {
     name: 'entrance to highroad (from landing)',
     altitude: 22.5,
     floors: [{ 
@@ -238,5 +291,90 @@ export default /* eslint-disable */
         ]
       }
     }]
+  }, {
+    name: 'landing, north access walkway',
+    altitude: 15,
+    incline: 2.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/u' },
+          { $ref: '#/def/t' },
+          { $ref: '#/def/s' },
+          { $ref: '#/def/k' }
+        ]
+      }
+    }],
+  }, {
+    name: 'lower plaza',
+    altitude: 7.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/V' },
+          { $ref: '#/def/W' },
+          { $ref: '#/def/X' },
+          { $ref: '#/def/Y' }
+        ]
+      }
+    }],
+  }, {
+    name: 'lower plaza walkway 0 of 4',
+    altitude: 7.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/Z0a' },
+          { $ref: '#/def/Z0b' },
+          { $ref: '#/def/Z0c' },
+          { $ref: '#/def/Z0d' }
+        ]
+      }
+    }],
+  }, {
+    name: 'lower plaza walkway 1 of 4',
+    altitude: 7.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/Z1a' },
+          { $ref: '#/def/Z1b' },
+          { $ref: '#/def/Z1c' },
+          { $ref: '#/def/Z1d' }
+        ]
+      }
+    }],
+  }, {
+    name: 'lower plaza walkway 2 of 4',
+    altitude: 7.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/Z2a' },
+          { $ref: '#/def/Z2b' },
+          { $ref: '#/def/Z2c' },
+          { $ref: '#/def/Z2d' }
+        ]
+      }
+    }],
+  }, {
+    name: 'lower plaza walkway 3 of 4',
+    altitude: 7.5,
+    floors: [{ 
+      outline: {
+        shape: 'polygon',
+        corners: [
+          { $ref: '#/def/Z3a' },
+          { $ref: '#/def/Z3b' },
+          { $ref: '#/def/Z3c' },
+          { $ref: '#/def/Z3d' }
+        ]
+      }
+    }],
   }]
 }
