513f6356dbd7770ef0257a41d57cab68278021b6
nicky-nym <nym.nicky@gmail.com>
2020-03-02 19:29:10 -0800

    issue #33: add "rollup" in package.json devDependencies, and add an "npm run rollup" script target

---
+++ rollup.config.js
@@ -0,0 +1,12 @@
+/** @file rollup.config.js
+ * @author Authored in 2020 at <https://github.com/nicky-nym/city3d>
+ * @license UNLICENSE
+ * This is free and unencumbered software released into the public domain.
+ * For more information, please refer to <http://unlicense.org>
+ */
+export default {
+  input: 'examples/sandbox.js',
+  output: {
+    file: 'build/city3d.js'
+  }
+}
